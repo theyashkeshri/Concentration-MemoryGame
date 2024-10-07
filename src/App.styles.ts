de3d26dbeb399b2f7bdcca5ad869d8fa293c7f58
Yash Keshri <125530408+csgoteresa@users.noreply.github.com>
2024-10-07 12:43:46 +0530

    Add files via upload

---
+++ src/App.styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+
+export const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(230px, 1fr));
+  grid-gap: 0.7rem;
+  justify-content: center;
+  align-items: center;
+  justify-items: center;
+`
